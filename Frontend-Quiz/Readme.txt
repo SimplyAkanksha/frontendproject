f7228b0c68a109d8683cef7a0ee56d460cfe03e8
SimplyAkanksha <100132507+SimplyAkanksha@users.noreply.github.com>
2022-06-19 18:07:21 +0530

    Add files via upload
    First Commit

---
+++ Frontend-Quiz/Readme.txt
@@ -0,0 +1,5 @@
+Name:Nadeem
+Github link : https://github.com/Nadeem7472/Frontend-Quiz
+
+The Online Test Application system creates an application that enables users to provide online tests, 
+review them, and display the results.
---
+++ Frontend-Quiz/js/quiz.js
@@ -0,0 +1,56 @@
+
+
+let quiz=[
+    {
+        question:"Which of the following is correct about TypeScript?",
+        option:[
+            "1.Angular is based on TypeScript",
+            "2.This is a superset of JavaScript",
+            "3.TypeScript is maintained by Microsoft",
+            "4.All of the Above",
+        ],
+        answer:4,
+    },
+    {
+        question:"Router is part of which of the following module?",
+        option:[
+            "1.@angular/core",
+            "2.@angular/router",
+            "3.Both",
+            "4.None of the Above",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"What is the decorator used for configuring your module class?",
+        option:[
+            "1.@NgModule",
+            "2.@NgApp",
+            "3.Both",
+           "4.None of the above",   
+        ],
+        answer:1,
+    },
+    {
+        question:"Interpolation in angular is done using ?",
+        option:[
+            "1.{{{}}}",
+            "2.[[]] ",
+            "3.{{}}",
+            "4.!!!!",   
+        ],
+        answer:3,
+
+    },
+    {
+        question:"To start working with Angular , we need to install the _____________components.",
+        option:[
+            "1.Editor",
+            "2.GIT",
+            "3.Npm",
+            "4.All of the above",   
+        ],
+        answer:4,
+    }
+]
